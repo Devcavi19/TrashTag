8ebb4822b2c869495a64da86aa19a9571b09a407
Devcavi19 <heavila@my.cspc.edu.ph>
2026-06-16 10:27:58 +0800

    add sampleRequest data structure

---
+++ src/data/sampleRequest.js
@@ -0,0 +1,11 @@
+const sampleRequest = {
+  id: '1718500000000',
+  photo: null,
+  gps: '14.5995° N, 120.9842° E',
+  type: 'Recyclable',
+  price: 30,
+  status: 'open',
+  postedAt: '2026-06-16T08:00:00.000Z',
+}
+
+export default sampleRequest
